36284868edbb595853d1f4d4c815c04e4d7307d8
Muhammad Amin Abdul Hamid <m.amin.a.hamid@gmail.com>
2025-06-09 17:03:22 +0800

    removed printed messages

--- test/beacon_broadcast_test.dart
+++ test/beacon_broadcast_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:beacon_broadcast/beacon_broadcast.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -63,96 +61,50 @@ void main() {
 
     test('when uuid is not set throws exception', () async {
       onStartMethodReturn(Future.value());
-      expect(
-          () => BeaconBroadcast()
-              .setMajorId(1)
-              .setMinorId(1)
-              .setTransmissionPower(-59)
-              .setIdentifier("identifier")
-              .start(),
+      expect(() => BeaconBroadcast().setMajorId(1).setMinorId(1).setTransmissionPower(-59).setIdentifier("identifier").start(),
           throwsA(isA<IllegalArgumentException>()));
     });
 
     test('when major id is not set throws exception', () async {
       onStartMethodReturn(Future.value());
-      expect(
-          () => BeaconBroadcast()
-              .setUUID("uuid")
-              .setMinorId(1)
-              .setTransmissionPower(-59)
-              .setIdentifier("identifier")
-              .start(),
+      expect(() => BeaconBroadcast().setUUID("uuid").setMinorId(1).setTransmissionPower(-59).setIdentifier("identifier").start(),
           throwsA(isA<IllegalArgumentException>()));
     });
 
     test('when minor id is not set throws exception', () async {
       onStartMethodReturn(Future.value());
-      expect(
-          () => BeaconBroadcast()
-              .setUUID("uuid")
-              .setMajorId(1)
-              .setTransmissionPower(-59)
-              .setIdentifier("identifier")
-              .start(),
+      expect(() => BeaconBroadcast().setUUID("uuid").setMajorId(1).setTransmissionPower(-59).setIdentifier("identifier").start(),
           throwsA(isA<IllegalArgumentException>()));
     });
 
-    test('when identifier and transmission power are not set starts normally',
-        () async {
+    test('when identifier and transmission power are not set starts normally', () async {
       onStartMethodReturn(Future.value());
-      expect(
-          () => BeaconBroadcast()
-              .setUUID("uuid")
-              .setMajorId(1)
-              .setMinorId(1)
-              .start(),
-          returnsNormally);
+      expect(() => BeaconBroadcast().setUUID("uuid").setMajorId(1).setMinorId(1).start(), returnsNormally);
     });
 
-    test('when extra data contains integer out of range throws exception',
-        () async {
+    test('when extra data contains integer out of range throws exception', () async {
       onStartMethodReturn(Future.value());
-      expect(
-          () => BeaconBroadcast().setUUID("uuid").setExtraData([270]).start(),
-          throwsA(isA<IllegalArgumentException>()));
+      expect(() => BeaconBroadcast().setUUID("uuid").setExtraData([270]).start(), throwsA(isA<IllegalArgumentException>()));
     });
 
     group('when custom layout is set', () {
       test('and minor id is not set returns normally', () async {
         onStartMethodReturn(Future.value());
         expect(
-            () => BeaconBroadcast()
-                .setUUID("uuid")
-                .setMajorId(1)
-                .setTransmissionPower(-59)
-                .setIdentifier("identifier")
-                .setLayout("layout")
-                .start(),
+            () => BeaconBroadcast().setUUID("uuid").setMajorId(1).setTransmissionPower(-59).setIdentifier("identifier").setLayout("layout").start(),
             returnsNormally);
       });
 
       test('and major id is not set returns normally', () async {
         onStartMethodReturn(Future.value());
         expect(
-            () => BeaconBroadcast()
-                .setUUID("uuid")
-                .setMinorId(1)
-                .setTransmissionPower(-59)
-                .setIdentifier("identifier")
-                .setLayout("layout")
-                .start(),
+            () => BeaconBroadcast().setUUID("uuid").setMinorId(1).setTransmissionPower(-59).setIdentifier("identifier").setLayout("layout").start(),
             returnsNormally);
       });
 
       test('and UUID is not set throws exception', () async {
         onStartMethodReturn(Future.value());
-        expect(
-            () => BeaconBroadcast()
-                .setMinorId(1)
-                .setTransmissionPower(-59)
-                .setIdentifier("identifier")
-                .setLayout("layout")
-                .start(),
+        expect(() => BeaconBroadcast().setMinorId(1).setTransmissionPower(-59).setIdentifier("identifier").setLayout("layout").start(),
             throwsA(isA<IllegalArgumentException>()));
       });
     });
@@ -161,29 +113,22 @@ void main() {
   group('checking if transmission is supported', () {
     test('when device returns 0 return BeaconStatus.supported', () async {
       onIsTransmissionSupportedMethodReturn(Future.value(0));
-      expect(await BeaconBroadcast().checkTransmissionSupported(),
-          BeaconStatus.supported);
+      expect(await BeaconBroadcast().checkTransmissionSupported(), BeaconStatus.supported);
     });
 
-    test('when device returns 1 return BeaconStatus.notSupportedMinSdk',
-        () async {
+    test('when device returns 1 return BeaconStatus.notSupportedMinSdk', () async {
       onIsTransmissionSupportedMethodReturn(Future.value(1));
-      expect(await BeaconBroadcast().checkTransmissionSupported(),
-          BeaconStatus.notSupportedMinSdk);
+      expect(await BeaconBroadcast().checkTransmissionSupported(), BeaconStatus.notSupportedMinSdk);
     });
 
     test('when device returns 2 return BeaconStatus.notSupportedBle', () async {
       onIsTransmissionSupportedMethodReturn(Future.value(2));
-      expect(await BeaconBroadcast().checkTransmissionSupported(),
-          BeaconStatus.notSupportedBle);
+      expect(await BeaconBroadcast().checkTransmissionSupported(), BeaconStatus.notSupportedBle);
     });
 
-    test(
-        'when device returns other value return BeaconStatus.notSupportedCannotGetAdvertiser',
-        () async {
+    test('when device returns other value return BeaconStatus.notSupportedCannotGetAdvertiser', () async {
       onIsTransmissionSupportedMethodReturn(Future.value(3));
-      expect(await BeaconBroadcast().checkTransmissionSupported(),
-          BeaconStatus.notSupportedCannotGetAdvertiser);
+      expect(await BeaconBroadcast().checkTransmissionSupported(), BeaconStatus.notSupportedCannotGetAdvertiser);
     });
   });
 
